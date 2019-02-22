5ca761fd1a5d9fc496ae7da43f280e2174231748
Matteo Neri <nremtt@gmail.com>
2019-02-22 19:26:09 +0100

    major changes in the probabilities. Added all the regular verbs using Conjugator

--- probar_verbos.py
+++ probar_verbos.py
@@ -6,23 +6,8 @@ from queue import deque
 from functools import reduce
 import bisect
 
-code_map = {'indicativo':{'conditional perfecto':'icp',
-                          'conditional simple':'ics',
-                          'futuro simple':'ifs',
-                          'presente':'ip',
-                          'preterito imperfecto':'ipi',
-                          'preterito perfecto compuesto':'ippc',
-                          'preterito perfecto simple':'ipps',
-                          'preterito pluscuamperfecto':'ip+'},
-            'subjuntivo':{'presente':'sp',
-                           'preterito imperfecto':'spi',
-                           'preterito imperfecto -ese':'spi2',
-                           'preterito perfecto':'spp',
-                           'preterito pluscuamperfecto':'sp+',
-                           'preterito pluscuamperfecto -ese':'sp+2'},
-            'imperativo':{'afirmativo':'ia'}}
 
-class conjugator:
+class Conjugator: 
     def __init__(self, verbo):
         self.infinitivo = verbo
         self.classe = verbo[-2:]
@@ -47,7 +32,7 @@ class conjugator:
             return [self.infinitivo[:-2]+d for d in desinencias[self.classe]]
         elif tempo=='conditional simple':
             desinencias = ['ía','ías','ía','íamos','íais','ían']
-            return [self.infinitivo+d for d in desinencias[self.classe]]
+            return [self.infinitivo+d for d in desinencias]
         elif tempo=='preterito perfecto compuesto':
             haber = ['he','has','ha','hemos','habéis','han']
             return ["{} {}".format(h,self.participio) for h in haber]
@@ -59,24 +44,24 @@ class conjugator:
             return ["{} {}".format(h,self.participio) for h in haber]
         elif tempo=='futuro simple':
             desinencias = ['é','ás','á','emos','éis','án']
-            return [self.infinitivo+d for d in desinencias[self.classe]]
+            return [self.infinitivo+d for d in desinencias]
 
     def subjuntivo(self, tempo):
         if tempo=='presente':
             desinencias = {'ar':['e','es','e','emos','éis','en'],
                            'er':['a','as','a','amos','áis','an'],
                            'ir':['a','as','a','amos','áis','an']}
-            return [self.infinitivo[-2]+d for d in desinencias[self.classe]]
+            return [self.infinitivo[:-2]+d for d in desinencias[self.classe]]
         elif tempo=='preterito imperfecto':
             desinencias = {'ar':['ara','aras','ara','áramos','arais','aran'],
                            'er':['iera','ieras','iera','iéramos','ierais','ieran'],
                            'ir':['iera','ieras','iera','iéramos','ierais','ieran']}
-            return [self.infinitivo[-2]+d for d in desinencias[self.classe]]
+            return [self.infinitivo[:-2]+d for d in desinencias[self.classe]]
         elif tempo=='preterito imperfecto -ese':
             desinencias = {'ar':['ase','ases','ase','ásemos','aseis','asen'],
                            'er':['iese','ieses','iese','iésemos','ieseis','iesen'],
                            'ir':['iese','ieses','iese','iésemos','ieseis','iesen']}
-            return [self.infinitivo[-2]+d for d in desinencias[self.classe]]
+            return [self.infinitivo[:-2]+d for d in desinencias[self.classe]]
         elif tempo=='preterito perfecto':
             haber = ['haya','hayas','haya','hayamos','hayáis','haian']
             return ["{} {}".format(h,self.participio) for h in haber]
@@ -88,17 +73,32 @@ class conjugator:
             return ["{} {}".format(h,self.participio) for h in haber]
 
     def imperativo(self, tempo):
-        if tempo='afirmativo':
+        if tempo=='afirmativo':
             desinencias = {'ar':['a','e','emos','ad','en'],
                            'er':['e','as','amos','ed','an'],
                            'ir':['e','as','amos','id','an']}
-            return [""]+[self.infinitivo[-2]+d for d in desinencias[self.classe]]
+            return [""]+[self.infinitivo[:-2]+d for d in desinencias[self.classe]]
 
-
-temp = [[(code,modo,tempo) for tempo,code in tempos.items()] for modo,tempos in code_map.items()]
-temp = reduce(lambda x,y: x.extend(y) or x, temp, [])
-code_map_inverse = {code:(modo,tempo) for code,modo,tempo in temp}
-verbs = os.listdir('verbos')
+code_map = {'indicativo':{'conditional perfecto':'icp',
+                          'conditional simple':'ics',
+                          'futuro simple':'ifs',
+                          'presente':'ip',
+                          'preterito imperfecto':'ipi',
+                          'preterito perfecto compuesto':'ippc',
+                          'preterito perfecto simple':'ipps',
+                          'preterito pluscuamperfecto':'ip+'},
+            'subjuntivo':{'presente':'sp',
+                           'preterito imperfecto':'spi',
+                           'preterito imperfecto -ese':'spi2',
+                           'preterito perfecto':'spp',
+                           'preterito pluscuamperfecto':'sp+',
+                           'preterito pluscuamperfecto -ese':'sp+2'},
+            'imperativo':{'afirmativo':'ia'}}
+_temp = [[(code,modo,tempo) for tempo,code in tempos.items()] for modo,tempos in code_map.items()]
+_temp = reduce(lambda x,y: x.extend(y) or x, _temp, [])
+code_map_inverse = {code:(modo,tempo) for code,modo,tempo in _temp}
+del _temp
+verbs = os.listdir('verbos_irregulares')+['reg_ar','reg_er','reg_ir']*1000
 stats = 'stats'
 
 if __name__=='__main__':
@@ -119,9 +119,10 @@ if __name__=='__main__':
         # random choice
         verb = np.random.choice(verbs)
         #mood = np.random.choice(['imperativo'])#['indicativo','subjuntivo','imperativo'] 
-        mood = np.random.choice(['indicativo','subjuntivo','imperativo'])
-        tempo = np.random.choice(os.listdir(os.path.join('verbos',verb,mood)))
-        verb_code = '{}_{}'.format(verb,code_map[mood][tempo])
+        mood = np.random.choice(['indicativo','subjuntivo','imperativo'],p=np.array([8,6,1])/sum([8,6,1]))
+        tempo = np.random.choice(os.listdir(os.path.join('verbos_irregulares','ir',mood)))
+        code = code_map[mood][tempo]
+        verb_code = '{}_{}'.format(verb,code)
     else:
         # choice with a prob proportional to the number of errors
         probs = np.array([(len(v)-sum(v)+0.2/len(v)) for v in s.values()])
@@ -137,27 +138,39 @@ if __name__=='__main__':
 
 
 
-    with open(os.path.join('verbos',verb,mood,tempo)) as f:
-        # ask for a verb conj
+    # ask for a verb conj
+    if verb in ['reg_ar','reg_er','reg_ir']:
+        with open('verbos_regulares/{}'.format(verb)) as reg_f:
+            verbs_meanings = [l for l in reg_f]
+            verb_meaning = np.random.choice(verbs_meanings)
+            verb, meaning = verb_meaning.strip().split(':')
+            print('{} - {}'.format(verb, meaning))
+            c = Conjugator(verb)
+            conj = c.__getattribute__(mood)(tempo)
+    else:
         print(verb)
-        print(mood, tempo)
-        print()
-        err = False
-        for p,v in zip(['yo','tu','el/ella','nosotros','vosotros','ell@s'],f):
-            if mood=='imperativo':
-                if p=='yo': continue
-            i = input('{} '.format(p))
-            if i!=v.strip():
-                print('No! {} {}'.format(p, v))
-                err=True
+        with open(os.path.join('verbos_irregulares',verb,mood,tempo)) as irr_f:
+            conj = [v for v in irr_f]
 
-        # change stat
-        temp = deque(s[verb_code],5)
-        temp.append(1-err)
-        s[verb_code] = list(temp)
-        #print(sum(temp)/len(temp))
-        with open(stats, 'w') as s_file:
-            json.dump(s, s_file)
+    print(mood, tempo)
+    print()
+    err = False
+
+    for p,v in zip(['yo','tu','el/ella','nosotros','vosotros','ell@s'],conj):
+        if mood=='imperativo':
+            if p=='yo': continue
+        i = input('{} '.format(p))
+        if i!=v.strip():
+            print('No! {} {}'.format(p, v))
+            err=True
+
+    # change stat
+    temp = deque(s[verb_code],5)
+    temp.append(1-err)
+    s[verb_code] = list(temp)
+    #print(sum(temp)/len(temp))
+    with open(stats, 'w') as s_file:
+        json.dump(s, s_file)
 
 
 
